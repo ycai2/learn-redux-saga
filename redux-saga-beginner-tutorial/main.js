80c1951c5cc897d65c18da3f4318f2998f3c6e42
Cai, Yisheng (CORP) <yisheng.cai@adp.com>
2017-12-07 14:07:53 -0500

    feature: add saga middleware to store

--- redux-saga-beginner-tutorial/main.js
+++ redux-saga-beginner-tutorial/main.js
@@ -3,11 +3,19 @@ import "babel-polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import { helloSaga } from './sagas'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(helloSaga)
 
 const action = type => store.dispatch({type})
 
